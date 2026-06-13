f1e341ef7aa602dedaad71a508c24e2707708c7b
shaikhfurquan <shaikhfurquan313@gmail.com>
2026-06-13 11:29:39 +0530

    transaction and ledger schema

---
+++ src/model/ledger.model.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose")
+
+const ledgerSchema = new mongoose.Schema({
+
+    account: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "account",
+        required: [true, "Ledger must be associated with a from account"],
+        index: true,
+        immutable: true
+    },
+    amount: {
+        type: Number,
+        required: [true, "Trasanction must be associated with a to account"],
+        immutable: true
+    },
+    transaction: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "transaction",
+        required: [true, "Ledger must be associated with a transaction"],
+        index: true,
+        immutable: true
+    },
+    type: {
+        type: String,
+        enum: {
+            values: ["CREDIT", "DEBIT"],
+            message: "Type can be either CREDIT OR DEBIT"
+        },
+        required: [true, "Ledger type is required"],
+        immutable: true
+    },
+
+}, { timestamps: true })
+
+function preventLedgerModification() {
+    throw new Error("Ledger entries are immutable and cannot be modified or deleted.")
+}
+ledgerSchema.pre('findOneAndUpdate', preventLedgerModification);
+ledgerSchema.pre('updateOne', preventLedgerModification);
+ledgerSchema.pre('deleteOne', preventLedgerModification);
+ledgerSchema.pre('remove', preventLedgerModification);
+ledgerSchema.pre('deleteMany', preventLedgerModification);
+
+
+const ledgerModel = mongoose.model("account", ledgerSchema)
+
+module.exports = ledgerModel
---
+++ src/model/transaction.model.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose")
+
+const transactionSchema = new mongoose.Schema({
+
+    fromAccount: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "account",
+        required: [true, "Trasanction must be associated with a from account"],
+        index: true
+    },
+    toAccount: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "account",
+        required: [true, "Trasanction must be associated with a to account"],
+        index: true
+    },
+    status: {
+        type: String,
+        enum: {
+            values: ["PENDING", "COMPLETED", "FAILED", "REVERSED"],
+            message: "Status either be PENDING , COMPLETED, FAILED OR REVERSED"
+        },
+        default: "PENDING"
+    },
+    amount: {
+        type: Number,
+        required: [true, "Amount is required for creating a transaction"],
+        min: [0, "Transaction amount cannot be negative"],
+    },
+    // unique identifier to avoid the twice transaction
+    idempotencyKey: {
+        type: String,
+        required: [true, "Idempotency key is required for creating the transaction"],
+        index: true,
+        unique: true
+    },
+
+
+
+}, { timestamps: true })
+
+// Compound index to optimize queries filtering by both user and status both
+transactionSchema.index({ user: 1, status: 1 })
+
+const TransactionModel = mongoose.model("account", transactionSchema)
+
+module.exports = TransactionModel
