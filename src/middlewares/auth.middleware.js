91750fe5e9f54f7b37f812f65de2a2f5af6f7f55
shaikhfurquan <shaikhfurquan313@gmail.com>
2026-06-18 17:38:43 +0530

    auth System User Middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken")
 
 const authMiddleware = async (req, res, next) => {
 
-     const token = req.cookies.token || req.headers.authorization?.split(" ")[ 1 ]
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[1]
 
     if (!token) {
         return res.status(401).json({
@@ -28,6 +28,33 @@ const authMiddleware = async (req, res, next) => {
     }
 }
 
+
+const authSystemUserMiddleware = async (req, res, next) => {
+
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[1]
+    if (!token) {
+        return res.status(401).json({
+            message: "Unauthorized access, token is missing"
+        })
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
+        const user = await UserModel.findById(decoded.userId).select("+systemUser")
+        if (!user.systemUser) {
+            return res.status(403).json({ message: "Forbidden access, not a system user" })
+        }
+        req.user = user
+        return next()
+
+    } catch (error) {
+        return res.status(401).json({
+            message: "Un-authorized access, token is invalid"
+        })
+    }
+}
+
 module.exports = {
-    authMiddleware
+    authMiddleware,
+    authSystemUserMiddleware
 }
