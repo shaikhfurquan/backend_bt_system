46f5b95b7377713c0c4d68b526ed67fb1ed2ea8e
shaikhfurquan <shaikhfurquan313@gmail.com>
2026-06-22 17:38:40 +0530

    get user account & get account balance

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -2,7 +2,7 @@ const UserModel = require("../model/user.model")
 const AccountModel = require("../model/account.model")
 const emailService = require('../services/email.service')
 
-
+// POST - create new account 
 const createAccountCtrl = async (req, res, next) => {
     try {
         const user = req.user
@@ -17,8 +17,48 @@ const createAccountCtrl = async (req, res, next) => {
     }
 }
 
+// GET - get all account of the logged-in user
+const getUserAccountsCtrl = async (req, res, next) => {
+    try {
+        const accounts = await AccountModel.find({
+            user: req.user._id
+        })
+        res.status(201).json({
+            accounts
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+// GET - get account balance
+const getAccountBalanceCtrl = async (req, res, next) => {
+    try {
+        const { accountId } = req.params
+        const account = await AccountModel.findOne({
+            _id: accountId,
+            user: req.user._id
+        })
+        if (!account) {
+            res.status(404).json({
+                message: "Account not found"
+            })
+        }
+        const balance = await account.getBalance()
+        res.status(201).json({
+            accountId:accountId,
+            balance
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
 
 
 module.exports = {
-    createAccountCtrl
+    createAccountCtrl,
+    getUserAccountsCtrl,
+    getAccountBalanceCtrl
 }
--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -4,7 +4,14 @@ const accountCtrl = require("../controllers/account.controller")
 
 const accountRoutes = express.Router()
 
+// POST - create new account 
 accountRoutes.post('/create', authMiddleware.authMiddleware, accountCtrl.createAccountCtrl)
+
+// GET - get all account of the logged-in user
+accountRoutes.get('/user-account', authMiddleware.authMiddleware, accountCtrl.getUserAccountsCtrl)
+
+// GET - get all account balance
+accountRoutes.get('/balance/:accountId', authMiddleware.authMiddleware, accountCtrl.getAccountBalanceCtrl)
 
 
 module.exports = accountRoutes
